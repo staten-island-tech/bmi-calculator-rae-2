72e325267afd6f2de0f9766785bbb17a4f188f80
rae-2 <cloudandelion@gmail.com>
2023-10-02 13:11:56 -0400

    aaaaa

---
+++ app.js
@@ -0,0 +1,17 @@
+/* function test(message){
+    console.log(message);
+}
+//string "" setntences, word etc.
+test("RAHHHH");
+//numbers/integers
+test(45);
+//booleans true or false
+test(true); */
+//decare variables, define variables
+const num1 = Number(prompt("enter a number"));
+const num2 = Number(prompt("enter a number"));
+
+function add(x,y){
+    console.log(x + y);
+}
+add(num1,num2);
